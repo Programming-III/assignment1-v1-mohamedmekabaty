f4e440091e937734b7dd70c7331a48829aa67598
mohamedmekabaty <90413573+mohamedmekabaty@users.noreply.github.com>
2025-11-12 15:16:34 +0200

    Enhance Animal class with display and feed methods
    Added methods for displaying and feeding the animal.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,27 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
-#include <string>
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
+#include <iostream>
 using namespace std;
-
-//define animal class here 
-
+#include <string>
 
 
-#endif
+class Animal{
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    void setName(string name);
+    void setAge(int age);
+    void setIsHungry(bool isHungry);
+    string getName();
+    int getAge();
+    bool getIsHungry();
+
+    void display();
+    void feed();
+};
+
+
+#endif // ANIMAL_H_INCLUDED
